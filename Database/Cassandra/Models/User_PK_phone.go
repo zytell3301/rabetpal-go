8a8a8b76631f0cf9b0b2b95042cad295fc4bc3dd
Arshiya Kiani <zytel3301@gmail.com>
2021-03-23 02:16:33 +0430

    Add necessary fields and methods

--- Database/Cassandra/Models/User_PK_phone.go
+++ Database/Cassandra/Models/User_PK_phone.go
@@ -1,1 +1,45 @@
 package Models
+
+import (
+	"github.com/gocql/gocql"
+	"rabetpal/Database/Cassandra"
+)
+
+var User_PK_phoneMetaData = Cassandra.TableMetaData{
+	Columns: map[string]struct{}{
+		"id":            {},
+		"name":          {},
+		"lastname":      {},
+		"username":      {},
+		"mobile":        {},
+		"phone":         {},
+		"address":       {},
+		"bank_accounts": {},
+		"national_code": {},
+		"profile_pic":   {},
+		"email":         {},
+		"user_type":     {},
+		"balance":       {},
+		"admin":         {},
+		"seller":        {},
+		"broker":        {},
+		"password":      {},
+	},
+	Pk:       map[string]struct{}{"phone": {}},
+	Ck:       nil,
+	Keyspace: "rabetpal",
+}
+
+func NewUserPKPhone(values map[string]interface{}, statement *gocql.Batch) bool {
+	switch statement == nil {
+	case true:
+		statement = Cassandra.ConnectionManager.GetSession("rabetpal").NewBatch(gocql.LoggedBatch)
+	}
+	Cassandra.AddId(&values)
+	error := Cassandra.ConnectionManager.GetSession("rabetpal").ExecuteBatch(statement)
+	switch error != nil {
+	case true:
+		return false
+	}
+	return true
+}
