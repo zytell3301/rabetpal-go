0bd67144dd5f26f0382c60c62466c7a68394943d
Arshiya Kiani <zytel3301@gmail.com>
2021-04-04 00:58:57 +0430

    Format codes for better readability

--- Auth/Auth.go
+++ Auth/Auth.go
@@ -2,11 +2,11 @@ package Auth
 
 import "github.com/dgrijalva/jwt-go"
 
-func KeyFunc(*jwt.Token) (interface{}, error){
-	return "123bac12415baf148291fb1241561291",nil
+func KeyFunc(*jwt.Token) (interface{}, error) {
+	return "123bac12415baf148291fb1241561291", nil
 }
 
-func DecodeJwt(signedString string) (jwt.MapClaims,error){
-	decodedJwt,err :=jwt.Parse(signedString,KeyFunc)
-	return decodedJwt.Claims.(jwt.MapClaims),err
+func DecodeJwt(signedString string) (jwt.MapClaims, error) {
+	decodedJwt, err := jwt.Parse(signedString, KeyFunc)
+	return decodedJwt.Claims.(jwt.MapClaims), err
 }
