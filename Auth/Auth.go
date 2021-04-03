7cabcaeefe0779ecd3325d5d998b34a23ce1556a
Arshiya Kiani <zytel3301@gmail.com>
2021-04-04 00:58:28 +0430

    Add DecodeJwt func

--- Auth/Auth.go
+++ Auth/Auth.go
@@ -5,3 +5,8 @@ import "github.com/dgrijalva/jwt-go"
 func KeyFunc(*jwt.Token) (interface{}, error){
 	return "123bac12415baf148291fb1241561291",nil
 }
+
+func DecodeJwt(signedString string) (jwt.MapClaims,error){
+	decodedJwt,err :=jwt.Parse(signedString,KeyFunc)
+	return decodedJwt.Claims.(jwt.MapClaims),err
+}
