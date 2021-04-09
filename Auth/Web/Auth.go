b10d95b45186343cdca6c2ff601f5339c8383d2f
Arshiya Kiani <zytell3301@gmail.com>
2021-04-10 00:07:53 +0430

    Proceed LoginUser method

--- Auth/Web/Auth.go
+++ Auth/Web/Auth.go
@@ -1,1 +1,21 @@
 package Web
+
+import (
+	"github.com/kataras/iris/v12"
+	"net/http"
+	"rabetpal/Auth"
+)
+
+func LoginUser(user map[string]interface{}, ctx iris.Context) error {
+	signedJwt, error := Auth.EncodeJwt(user)
+	switch error != nil {
+	case true:
+		return error
+	}
+	ctx.SetCookie(&http.Cookie{
+		Name:     "user",
+		Value:    signedJwt,
+		HttpOnly: true,
+	})
+	return nil
+}
