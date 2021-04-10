a8749fa2e26d92e190a64be1cd7f8c356c36f3d8
Arshiya Kiani <zytell3301@gmail.com>
2021-04-10 13:53:57 +0430

    Implement GetUser method

--- Auth/Web/Auth.go
+++ Auth/Web/Auth.go
@@ -1,6 +1,7 @@
 package Web
 
 import (
+	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/kataras/iris/v12"
 	"net/http"
 	"rabetpal/Auth"
@@ -18,4 +19,18 @@ func LoginUser(user map[string]interface{}, ctx iris.Context) error {
 		HttpOnly: true,
 	})
 	return nil
+}
+
+func GetUser(ctx iris.Context) (jwt.MapClaims, error) {
+	signedString := ctx.GetCookie("user")
+	switch signedString == "" {
+	case true:
+		return nil, nil
+	}
+	user, error := Auth.DecodeJwt(signedString)
+	switch error != nil || user == nil {
+	case true:
+		return nil, error
+	}
+	return user.Claims.(jwt.MapClaims), nil
 }
