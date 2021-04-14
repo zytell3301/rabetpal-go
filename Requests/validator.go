9283851970f2186d01021861f2cd0014ec97e131
zytell3301 <zytell3301@gmail.com>
2021-04-15 02:20:42 +0430

    Proceed Validate func

--- Requests/validator.go
+++ Requests/validator.go
@@ -1,1 +1,21 @@
 package Requests
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+var validatorPackage = validator.New()
+
+func Validate(data map[string]interface{}, rules map[string]string) bool {
+	for offset, rule := range rules {
+		value, isSet := data[offset]
+		switch isSet {
+		case true:
+			validatorPackage.Var(value, rule)
+			break
+		default:
+			validatorPackage.Var(nil, rule)
+		}
+	}
+	return true
+}
