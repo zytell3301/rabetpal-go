d5ec641c570df2a53c90133c85ad3dbc52defcf6
zytell3301 <zytell3301@gmail.com>
2021-04-22 23:32:22 +0430

    Initiate configs on app startup

---
+++ Database/Uuid/config.go
@@ -0,0 +1,22 @@
+package Uuid
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+var Configs *viper.Viper
+
+func init() {
+	fmt.Println("Uuid Configs are being read")
+	Configs = viper.New()
+	Configs.AddConfigPath("./Configs")
+	Configs.SetConfigName("Uuid")
+	Configs.SetConfigType("yaml")
+	err := Configs.ReadInConfig()
+	switch err != nil {
+	case true:
+		panic("Unable to read config \"Uuid\". Error: " + err.Error())
+	}
+	fmt.Println("Config \"Uuid\" just got read successfully")
+}
