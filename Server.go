cb0b6f86d6028dc2a7783a889fa59e2989376cff
zytell3301 <zytell3301@gmail.com>
2021-04-23 00:15:07 +0430

    Add user register route

--- Server.go
+++ Server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/kataras/iris/v12"
 	"rabetpal/Controllers"
+	"rabetpal/Controllers/Users"
 	_ "rabetpal/Database/Cassandra/Keyspaces"
 	_ "rabetpal/Database/Uuid"
 	_ "rabetpal/Database/Uuid/Generator"
@@ -12,6 +13,7 @@ func main() {
 	r := iris.New()
 
 	r.Get("/test",Controllers.HomeController.Index)
+	r.Post("/register",Users.UsersController.Register)
 
 	r.Listen(":4042")
 }
