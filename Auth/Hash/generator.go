bf5cf0943870fce9c00ab082f318b4518d77b260
zytell3301 <zytell3301@gmail.com>
2021-04-26 01:10:45 +0430

    Cleanup code

--- Auth/Hash/generator.go
+++ Auth/Hash/generator.go
@@ -2,7 +2,7 @@ package Hash
 
 import "golang.org/x/crypto/bcrypt"
 
-func GenerateHash(password string) (string) {
+func GenerateHash(password string) string {
 	hash, _ := bcrypt.GenerateFromPassword([]byte(password), Configs.GetInt("hash_round"))
 	return string(hash)
 }
