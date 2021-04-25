952183f0fddb1043f93e671e2b9990495f18ee59
zytell3301 <zytell3301@gmail.com>
2021-04-26 01:15:58 +0430

    Add Hash configs for initiation and also order imports

--- Server.go
+++ Server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/kataras/iris/v12"
+	_ "rabetpal/Auth/Hash"
 	"rabetpal/Controllers"
 	"rabetpal/Controllers/Users"
 	_ "rabetpal/Database/Cassandra/Keyspaces"
@@ -12,8 +13,8 @@ import (
 func main() {
 	r := iris.New()
 
-	r.Get("/test",Controllers.HomeController.Index)
-	r.Post("/register",Users.UsersController.Register)
+	r.Get("/test", Controllers.HomeController.Index)
+	r.Post("/register", Users.UsersController.Register)
 
 	r.Listen(":4042")
 }
