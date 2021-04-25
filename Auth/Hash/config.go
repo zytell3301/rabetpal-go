7abe316e97fd02228663bdd550214bc8a765f908
zytell3301 <zytell3301@gmail.com>
2021-04-26 00:17:40 +0430

    Initiate Hash configs on app startup

---
+++ Auth/Hash/config.go
@@ -0,0 +1,22 @@
+package Hash
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+var Configs *viper.Viper
+
+func init() {
+	fmt.Println("Hash configs are being read")
+	Configs := viper.New()
+	Configs.SetConfigType("yaml")
+	Configs.SetConfigName("Hash")
+	Configs.AddConfigPath("./Configs")
+	err := Configs.ReadInConfig()
+	switch err != nil {
+	case true:
+		panic("Unable to read config \"Cassandra\". Error: " + err.Error())
+	}
+	fmt.Println("Hash configs just got set successfully")
+}
