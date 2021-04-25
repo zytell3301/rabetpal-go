28717f09b585d5aec165d9e36af01d81c55100ed
zytell3301 <zytell3301@gmail.com>
2021-04-26 01:10:18 +0430

    Fix a minor bug and correct a logging message

--- Auth/Hash/config.go
+++ Auth/Hash/config.go
@@ -9,14 +9,14 @@ var Configs *viper.Viper
 
 func init() {
 	fmt.Println("Hash configs are being read")
-	Configs := viper.New()
+	Configs = viper.New()
 	Configs.SetConfigType("yaml")
 	Configs.SetConfigName("Hash")
 	Configs.AddConfigPath("./Configs")
 	err := Configs.ReadInConfig()
 	switch err != nil {
 	case true:
-		panic("Unable to read config \"Cassandra\". Error: " + err.Error())
+		panic("Unable to read config \"Hash\". Error: " + err.Error())
 	}
 	fmt.Println("Hash configs just got set successfully")
 }
