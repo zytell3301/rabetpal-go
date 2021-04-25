ca6693034c68ae680bd86007dd02c2ca864f5180
zytell3301 <zytell3301@gmail.com>
2021-04-26 00:39:26 +0430

    Remove unnecessary error return in  GenerateHash method

--- Auth/Hash/generator.go
+++ Auth/Hash/generator.go
@@ -2,7 +2,7 @@ package Hash
 
 import "golang.org/x/crypto/bcrypt"
 
-func GenerateHash(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), Configs.GetInt("hash_round"))
-	return string(hash), err
+func GenerateHash(password string) (string) {
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), Configs.GetInt("hash_round"))
+	return string(hash)
 }
