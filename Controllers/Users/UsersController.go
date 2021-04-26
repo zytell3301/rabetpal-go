fac23415dc9bee6dccf80878fc8b580e819cb93d
zytell3301 <zytell3301@gmail.com>
2021-04-27 02:04:49 +0430

    Proceed Register method

--- Controllers/Users/UsersController.go
+++ Controllers/Users/UsersController.go
@@ -1,11 +1,36 @@
 package Users
 
-import "github.com/kataras/iris/v12"
+import (
+	"fmt"
+	"github.com/kataras/iris/v12"
+	"rabetpal/Auth/Hash"
+	"rabetpal/Database/Cassandra/Models"
+	"rabetpal/Requests"
+	"rabetpal/Requests/Rules/UsersRequests"
+)
 
 var UsersController = struct {
 	Register func(ctx iris.Context)
 }{
 	Register: func(ctx iris.Context) {
+		validationErrors := Requests.ValidateForm(ctx, UsersRequests.RegisterRequest)
+		switch len(validationErrors) == 0 {
+		case false:
+			fmt.Println("Validation failed. Errors:",validationErrors)
+			return
+		}
 
+		Models.NewUser(map[string]interface{}{
+			"name":          ctx.FormValue("name"),
+			"lastname":      ctx.FormValue("lastname"),
+			"username":      ctx.FormValue("username"),
+			"mobile":        ctx.FormValue("mobile"),
+			"phone":         ctx.FormValue("phone"),
+			"national_code": ctx.FormValue("national_code"),
+			"email":         ctx.FormValue("email"),
+			"password":      Hash.GenerateHash(ctx.FormValue("password")),
+			"user_type":     "USER",
+			"balance":       0,
+		}, nil)
 	},
 }
