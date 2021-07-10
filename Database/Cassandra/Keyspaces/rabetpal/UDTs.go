187402df7e9d9c1acaf5629df36cc6596116c3e2
zytell3301 <zytell3301@gmail.com>
2021-07-11 00:03:26 +0430

    Update rabetpal UDTs

--- Database/Cassandra/Keyspaces/rabetpal/UDTs.go
+++ Database/Cassandra/Keyspaces/rabetpal/UDTs.go
@@ -1,1 +1,11 @@
 package rabetpal
+
+import (
+	"time"
+)
+
+type BankAccounts struct {
+	AccountNumber  string    `cql:"account_number"`
+	Iban           string    `cql:"iban"`
+	ExpirationDate time.Time `cql:"expiration_date"`
+}
